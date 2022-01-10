2d249720ce17e7b50b21122471980ecca5f00a03
Faris-Hmd <bluocat4@gmail.com>
2022-01-10 14:51:55 +0200

    edit for build

--- src/component/ItemFunc.jsx
+++ src/component/ItemFunc.jsx
@@ -1,11 +1,5 @@
 /** @format */
-import { useState, useEffect } from "react";
-
 const ItemFunc = (props) => {
-  let [qu, setQu] = useState(0);
-
-  useEffect(() => {}, [qu]);
-
   const handleQuShiftClass = () => {
     return props.qu === 0 ? "Shift" : "";
   };
@@ -26,11 +20,11 @@ const ItemFunc = (props) => {
       </div>
       <div className="counter-d">
         <div className="decrease" onClick={props.handleIncr}>
-          <i className="fa fa-angle-left">+</i>
+          <i className="fa fa-angle-left"></i>
         </div>
 
         <div className="increase" onClick={props.handleDecr}>
-          <i className="fa fa-angle-right">-</i>
+          <i className="fa fa-angle-right"></i>
         </div>
       </div>
     </div>
