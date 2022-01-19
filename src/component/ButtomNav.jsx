3e92bf6c50299d9587b26b0ce1f1910b7b710760
Faris-Hmd <bluocat4@gmail.com>
2022-01-19 21:28:20 +0200

    buttom nav

--- src/component/ButtomNav.jsx
+++ src/component/ButtomNav.jsx
@@ -9,15 +9,23 @@ const ButtomNav = () => {
       <div className="buttomNav">
         <Link to="/menu" className="Link buttomNavLink">
           <i className=" fa fa-home fa-2x"></i>
+          <div className="disc">Home</div>
         </Link>
         <Link to="/fav" className="Link buttomNavLink">
-          <i className=" fa fa-home fa-2x"></i>
+          <i className=" fa fa-shopping-cart fa-2x"></i>
+          <div className="disc">Home</div>
         </Link>
         <Link to="cart" className="Link buttomNavLink">
           <i className=" fa fa-home fa-2x"></i>
+          <div className="disc">Deliverly</div>
+        </Link>
+        <Link to="cart" className="Link buttomNavLink">
+          <i className=" fa fa-home fa-2x"></i>
+          <div className="disc">Deliverly</div>
         </Link>
         <Link to="profile" className="Link buttomNavLink">
-          <i className=" fa fa-home fa-2x"></i>
+          <i className=" fa fa-user fa-2x"></i>
+          <div className="disc">Profile</div>
         </Link>
       </div>
     </div>
